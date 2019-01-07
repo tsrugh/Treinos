a24e21f11559005a7eb21dee3031e063cb5501d6
Lucas Pereira <lucas_3600@hotmail.com.br>
2019-01-07 19:59:09 -0200

    Termino de comentarios do webservice

--- webservice2/index.php
+++ webservice2/index.php
@@ -11,28 +11,40 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 require './src/crud.php';
 
-//
+//Criando uma instancia de App
 $app = new \Slim\App;
+
+// Cirando a url e exibindo o resultado da consulta por determinado treino
 $app->get('/teste/{treino}', function (Request $request, Response $response, array $args) {
+    
+    // Variavel recebe o valor do parametro passado
     $treino = $request->getAttribute('treino');
 
+    // Objeto que recebe a classe Crud
     $conn = new Crud();
 
+    // Exibindo o resultado para o cliente
     echo $conn->buscaPorTreino($treino);
 });
 
+// Url que retorna todos os nomes dos treinos
 $app->get('/treinos', function (Request $request, Response $response, array $args) {
 
+     // Objeto que recebe a classe Crud
     $conn = new Crud();
 
+    // Exibindo o resultado para o cliente
     echo $conn->nomeTreinos();
 
 });
 
+// Url que retorna e exibe todos os exercicios de cada treino
 $app->get('/todos', function (Request $request, Response $response, array $args) {
 
+    // Objeto que recebe a classe Crud
     $conn = new Crud();
 
+    // Exibindo o resultado para o cliente
     echo $conn->buscaGeral();
 
 });
